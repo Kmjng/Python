5a75c8b9d1e2c79b4df1d6ad90f14d9c296bdfb9
Minjeong Kim <154487112+Kmjng@users.noreply.github.com>
2024-01-05 22:54:39 +0900

    Create Basic0105.py

---
+++ .github/PracticePandas/Basic0105.py
@@ -0,0 +1,16 @@
+#시리즈 만들어서 데이터프레임 만들기
+import pandas as pd
+
+type(data_frame.날짜) #결과값: pandas.core.series.Series
+
+lst_num = [1,2,3,4,5]
+s1 = pd.core.series.Series(lst_num)
+print(s1) #이렇게 하면 아래와 같이 출력된다.
+# 0    1
+# 1    2
+# 2    3
+# 3    4
+# 4    5
+# dtype: int64
+s2=pd.core.series.Series(['one','two','three','four','five'])
+pd.DataFrame(data=dict(num=s1, word=s2))
